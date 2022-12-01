8dff00395bf1ea4df7d1d576afef49282c9b3d87
Planck35 <chaoyuw1997@gmail.com>
2022-11-30 22:39:09 -0800

    Check in workable version of cpp parser.

--- demo.cpp
+++ demo.cpp
@@ -1,3 +1,16 @@
-//
-// Created by chaoy on 2022/11/30.
-//
+#include "csv.h"
+#include <string>
+#include <iostream>
+#include "config.h"
+
+int main() {
+  std::string source_dir(PROJECT_SOURCE_DIR);
+  io::CSVReader<3> csv_reader(source_dir + "/ram.csv");
+  csv_reader.read_header(io::ignore_missing_column, "vendor", "size", "speed");
+  std::string vendor;
+  int size;
+  double speed;
+  while (csv_reader.read_row(vendor, size, speed)) {
+    std::cout << "vendor:" <<  vendor << ",size:" << size << ",speed:" << speed << std::endl;
+  }
+}
